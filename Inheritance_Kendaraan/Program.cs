eea552f33bc59bca75c0e25d35ddba8311a664f6
afifmaulanaanam1-cmd <afifmaulanaanam1@gmail.com>
2026-04-02 13:56:08 +0700

    Sistem Kendaraan Mobil dengan menggunakan Inheritance

--- Inheritance_Kendaraan/Program.cs
+++ Inheritance_Kendaraan/Program.cs
@@ -1,6 +1,14 @@
 ﻿
 
-Kendaraan kendaraanObj = new Kendaraan();
+Mobil kendaraanObj = new Mobil();
+
+kendaraanObj.Merk = "Toyota";
+kendaraanObj.Tahun = 2021;
+kendaraanObj.JumlahPintu = 4;
+
+Console.WriteLine($"Mobil saya adalah {kendaraanObj.Merk} pengeluaran tahun {kendaraanObj.Tahun} dan mempunyai pintu {kendaraanObj.JumlahPintu}");
+
+kendaraanObj.Jalan();
 
 class Kendaraan
 {
@@ -17,6 +25,7 @@ class Mobil : Kendaraan
 {
     public int JumlahPintu;
 }
+
 
 
 
